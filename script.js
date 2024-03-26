e7dbfcf80e878626b314c712b455f029bdeaaa4e
nackho <nick_ho1011@yahoo.com>
2024-03-26 16:18:39 -0700

    Added Edit button

--- script.js
+++ script.js
@@ -9,9 +9,36 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = entryBox.value;
         taskList.appendChild(li);
+
+        let task_edit = document.createElement("button");
+        task_edit.classList.add("edit");
+        task_edit.innerHTML = "Edit";
+        taskList.appendChild(task_edit);
+        
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     entryBox.value = '';
+    saveData()
 }
+
+taskList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("complete");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", taskList.innerHTML);
+}
+
+function showTask(){
+    taskList.innerHTML = localStorage.getItem("data");
+}
+showTask();
